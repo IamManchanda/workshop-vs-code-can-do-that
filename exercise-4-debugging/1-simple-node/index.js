33f67ec2a69cf88ceb84775258c05b9861e09577
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-21 22:58:34 +0530

    debugging practice!

--- exercise-4-debugging/1-simple-node/index.js
+++ exercise-4-debugging/1-simple-node/index.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const API_BASE = "https://lifx-lamp-api.azurewebsites.net/api";
+const { API_BASE } = process.env;
 
 const App = {
   /**
@@ -8,16 +8,15 @@ const App = {
   async init() {
     const result = axios
       .get(`${API_BASE}/setColor?color=blue`)
-      .then(response => {
-        response.data.results.forEach(item => {
-          console.log(item);
+      .then((response) => {
+        response.data.results.forEach((item) => {
           // update the lamp color
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("Unable to connect to the LAMP API");
       });
-  }
+  },
 };
 
 App.init();
